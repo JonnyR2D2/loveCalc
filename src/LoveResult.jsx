d2409cd93c011372bb323f4fb11b355888448d33
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2022-12-09 13:51:03 +0000

    I added some phone specs

--- src/LoveResult.jsx
+++ src/LoveResult.jsx
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 import "./LoveResult.css";
 import sexyThanosImg from "./Imgs/yg70lgwk64d91.jpg";
 import unsexyThanosImg from "./Imgs/DZ-JQccWAAApmCX.jpg";
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function LoveResult({ firstName, secondName }) {
   const lovePercentage = Math.floor(Math.random()*100);
   const img = lovePercentage <= 49 ? unsexyThanosImg : sexyThanosImg;
+  const navigate = useNavigate();
+
+  const onEnter = (event) => {
+    event.preventDefault();
+    navigate("/");
+  };
 
   return (
     <>
@@ -30,6 +37,7 @@ function LoveResult({ firstName, secondName }) {
         </p>
         <div >
           <img className="sweetSweetThanos" id = "sweetSweetThanos" src = {img}/>
+          <input type="Submit" value="try Again" onClick={onEnter} />
         </div>
       </div>
     </>
